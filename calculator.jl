44d6a2335a24fb68d033854da8b795ee0826fcc4
josemibnf <josemi.bnf@gmail.com>
2021-04-25 09:27:34 +0200

    .

---
+++ calculator.jl
@@ -0,0 +1,13 @@
+function sum(x, y)
+    return x+y
+end
+
+suma(x,y) = x+y
+
+resta(x,y) = x-y
+
+print("Selecciona una opcion. \n")
+print("👉 SUMA.")
+print("👉 RESTA.")
+print("👉 SUMA.")
+print("👉 SUMA.")
